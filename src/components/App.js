c4b3b34df482c62b2863dbce64b06c2bbc2505cc
Sonu <sonu3660@gmail.com>
2020-08-19 01:11:55 +0530

    Add UI

--- src/components/App.js
+++ src/components/App.js
@@ -1,7 +1,26 @@
 import React from "react";
+import { data } from "../data";
+import Navbar from "./Navbar";
+import MovieCard from "./MovieCard";
+import "../index.css";
 
 const App = () => {
-   return <div>App</div>;
+   return (
+      <div className="App">
+         <Navbar />
+         <div className="main">
+            <div className="tabs">
+               <div className="tab">Movies</div>
+               <div className="tab">Favorites</div>
+            </div>
+            <div className="list">
+               {data.map((movie) => (
+                  <MovieCard movie={movie} />
+               ))}
+            </div>
+         </div>
+      </div>
+   );
 };
 
 export default App;
