ba9e4fbdc120a7f17928268c8e3d71360d87f2f6
Sonu <sonu3660@gmail.com>
2020-08-19 02:30:22 +0530

    Creating the store

--- src/index.js
+++ src/index.js
@@ -7,7 +7,7 @@ import movies from "./reducers";
 
 // Create store and pass reducers
 const store = createStore(movies);
-console.log("store", store);
-console.log("State", store.getState());
+// console.log("store", store);
+// console.log("State", store.getState());
 
 ReactDOM.render(<App />, document.querySelector("#root"));
