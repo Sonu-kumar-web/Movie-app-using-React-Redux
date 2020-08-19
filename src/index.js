44493f9e59e29eeebd3c53fa27d10da7241ac73e
Sonu <sonu3660@gmail.com>
2020-08-20 01:44:04 +0530

    Add Middleware

--- src/index.js
+++ src/index.js
@@ -1,12 +1,24 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import { createStore } from "redux";
+import { createStore, applyMiddleware } from "redux";
 
 import App from "./components/App";
 import rootReducer from "./reducers";
 
+// function logger(obj, next, action)
+// logger(obj)(next)(action)  -> In currying form
+const logger = function ({ dispatch, getState }) {
+   return function (next) {
+      return function (action) {
+         // Middleware code
+         console.log("ACTION_TYPE=", action.type);
+         next(action);
+      };
+   };
+};
+
 // Create store and pass reducers
-const store = createStore(rootReducer);
+const store = createStore(rootReducer, applyMiddleware(logger));
 // console.log("store", store);
 console.log("Before state", store.getState());
 
